39fd311c3f909cf72148183d48981ae2d4675e65
Sushantmg <sushantmg2004@gmail.com>
2025-12-09 17:59:05 +0545

    global types

--- types/global-types.ts
+++ types/global-types.ts
@@ -1,16 +1,10 @@
 import { Request } from "express";
 
-// -------------------------
-// JWT Payload Type
-// -------------------------
 export interface AuthPayload {
   userId: string;
-  role: "USER" | "STAFF" | "SUPERUSER";
+  role: "ADMIN" | "TEACHER" | "STUDENT" | "STAFF" | "SUPERUSER";
 }
 
-// -------------------------
-// Extend Express Request
-// -------------------------
 export interface RequestWithUser extends Request {
   user?: AuthPayload;
 }
